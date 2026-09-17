71e802caf1cfb17d69475f5cb3e1b79139125855
Michael Manning <mmanning2@zagmail.gonzaga.edu>
2026-09-17 12:13:36 -0700

    get input from user to calculate work and power

---
+++ TinkeringFun/variables_lab.py
@@ -0,0 +1,12 @@
+force = float(input("Enter a force in newtons: "))
+distance = float(input("Enter a distance in meters: "))
+
+work = force * distance
+print("work:", work)
+print(type(work))
+
+time = 6
+power = work / time
+print("power:", power)
+print(type(power))
+
